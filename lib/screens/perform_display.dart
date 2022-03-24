d7329e744b590447c206f9875116e4335d4b7446
IcyFeVA <zirn.pascal@gmail.com>
2022-03-23 18:34:57 -0700

    always on screen done

--- lib/screens/perform_display.dart
+++ lib/screens/perform_display.dart
@@ -1,6 +1,6 @@
 import 'package:WikiMagic/services/database.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';
 import 'package:WikiMagic/helpers/helpers.dart';
 import 'package:provider/provider.dart';
@@ -17,40 +17,78 @@ class PerformDisplay extends StatefulWidget {
 class _PerformDisplayState extends State<PerformDisplay> {
   @override
   Widget build(BuildContext context) {
-
     final user = Provider.of<MyUser>(context);
 
     return StreamBuilder<DocumentSnapshot>(
-      stream: DatabaseService(uid: user.uid).userData,
-      builder: (context, snapshot) {
-        if(snapshot.hasData) {
+        stream: DatabaseService(uid: user.uid).userData,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final Map<String, dynamic> doc =
+                snapshot.data?.data() as Map<String, dynamic>;
 
-          final Map<String, dynamic> doc = snapshot.data?.data() as Map<String, dynamic>;
+            String txt1 = snapshot.data?.get('searchterm');
+            String txt2 = snapshot.data?.get('selection');
 
-          return Scaffold(
-            body: GestureDetector(
-              behavior: HitTestBehavior.translucent,
-              onDoubleTap: () {
-                if(widget.toggle != null) {
-                  widget.toggle!();
-                }
-              },
-              child: SafeArea(
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text('${snapshot.data?.get('searchterm')}'),
-                      Text('${snapshot.data?.get('selection')}'),
-                    ],
+            if (txt1 == '-') {
+              txt1 = 'Upcoming Alarm';
+            }
+            if (txt2 == '-') {
+              txt2 = 'Tomorrow 8:00 AM';
+            }
+
+            return Scaffold(
+              body: GestureDetector(
+                behavior: HitTestBehavior.translucent,
+                onDoubleTap: () {
+                  if (widget.toggle != null) {
+                    widget.toggle!();
+                  }
+                },
+                child: Scaffold(
+                  backgroundColor: Colors.black,
+                  body: SafeArea(
+                    child: Center(
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(height: 160),
+                          Column(
+                            children: <Widget>[
+                              Container(
+                                child: Text(
+                                    DateFormat("hh").format(DateTime.now()),
+                                    style: TextStyle(
+                                        color: Colors.white10,
+                                        fontSize: 80,
+                                        fontFamily: 'Roboto')),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(bottom: 80.0),
+                                child: Text(
+                                    DateFormat("mm").format(DateTime.now()),
+                                    style: TextStyle(
+                                        height: 0.8,
+                                        color: Colors.white10,
+                                        fontSize: 80,
+                                        fontFamily: 'Roboto')),
+                              ),
+                            ],
+                          ),
+                          Icon(Icons.alarm, color: Colors.white10, size: 22),
+                          Text(txt1,
+                              style: TextStyle(
+                                  color: Colors.white10, height: 1.7)),
+                          Text(txt2,
+                              style: TextStyle(
+                                  color: Colors.white10, fontSize: 12)),
+                        ],
+                      ),
+                    ),
                   ),
                 ),
               ),
-            ),
-          );
-        }
-        return Container();
-      }
-    );
+            );
+          }
+          return Container();
+        });
   }
 }
